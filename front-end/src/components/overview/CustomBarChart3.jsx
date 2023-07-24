ed2f9bad71361f1143659f223f7205bfc638c2c4
Deep Chandra <deepc@andrew.cmu.edu>
2023-07-24 04:17:46 +0300

    zoom feature implemented

--- front-end/src/components/overview/CustomBarChart3.jsx
+++ front-end/src/components/overview/CustomBarChart3.jsx
@@ -1,28 +1,14 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { Card, Grid, Title, BarChart, Button } from "@tremor/react";
+import { Card, Grid, Title, BarChart, Button, Legend } from "@tremor/react";
 import { Dialog, Transition } from "@headlessui/react";
-import { ArrowsExpandIcon, XIcon } from "@heroicons/react/outline";
+import { ArrowsExpandIcon, XIcon, PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/outline";
 
 const data2c = require('../../data/data2c.json');
 const predefinedColors = [
-  "red",
-  "lime",
-  "indigo",
-  "orange",
   "teal",
-  "violet",
-  "amber",
-  "cyan",
-  "pink",
-  "green",
   "sky",
-  "purple",
-  "emerald",
-  "blue",
   "rose",
-  "slate",
-  "yellow",
-  "fuchsia"
+  "violet",
 ];
 
 const dataByPI = data2c.reduce((groups, row) => {
@@ -38,14 +24,15 @@ const CustomBarChart3 = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [genomeColors, setGenomeColors] = useState({});
 
-  const [zoom, setZoom] = useState(1); // 1 is the default zoom level
-  const zoomIn = () => setZoom(prevZoom => Math.max(prevZoom - 1, 1)); // Adjust these numbers as needed
-  const zoomOut = () => setZoom(prevZoom => Math.min(prevZoom + 1, 5)); // Adjust these numbers as needed
+  const [rowSize, setRowSize] = useState(4);
+  const plus = () => setRowSize(prevZoom => Math.max(prevZoom - 1, 1)); // Adjust these numbers as needed
+  const minus = () => setRowSize(prevZoom => Math.min(prevZoom + 1, 10)); // Adjust these numbers as needed
 
-  // Calculate number of items in grid and card size based on zoom level
-  const numItemsInGrid = 3 + zoom; // Adjust this calculation as needed
-  const cardSize = ``; // Adjust this calculation as needed
-  console.log(`cardSize: ${cardSize}`);
+  // Calculate number of items in grid and card size based on rowSize level
+  const numItemsInGrid = rowSize //+ rowSize; // Adjust this calculation as needed
+
+  var cardSize = "w-auto h-[12vh]"; // Adjust this calculation as needed
+  const genomeNames = [...new Set(data2c.map(item => item.genome))];
   useEffect(() => {
     const genomeNames = [...new Set(data2c.map(item => item.genome))];
     const genomeColorMapping = genomeNames.reduce((acc, genome, index) => {
@@ -55,11 +42,17 @@ const CustomBarChart3 = () => {
     setGenomeColors(genomeColorMapping);
   }, []);
 
+  useEffect(() => { setRowSize(4) }, [isOpen]);
+
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
 
   return (
     <div className='flex flex-col space-y-4'>
+      <Legend className='flex justify-end'
+        categories={genomeNames}
+        colors={predefinedColors}
+      />
       <div className='flex justify-between space-x-2'>
         {Object.entries(dataByPI)
           // limit to first 3 PIs when the modal is closed
@@ -86,7 +79,7 @@ const CustomBarChart3 = () => {
               <Card key={pi}>
                 <Title>{pi}</Title>
                 <BarChart
-                  className='h-[91%]'
+                  className=''
                   data={chartData}
                   index="name"
                   categories={Object.keys(genomeColors)}
@@ -99,6 +92,7 @@ const CustomBarChart3 = () => {
               </Card>
             );
           })}
+
       </div>
       <Button
         icon={ArrowsExpandIcon}
@@ -131,10 +125,13 @@ const CustomBarChart3 = () => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <div className="inline-block align-bottom bg-white rounded-sm text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle max-w-[90vw] w-full">
-                <div className="bg-white p-10 overflow-y-auto h-[85vh] mx-auto max-w-full">
-
-                  <Grid numItems={numItemsInGrid} className="gap-4">
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle max-w-[90vw] w-full">
+                <div className="bg-white p-4 overflow-y-auto h-[85vh] mx-auto max-w-full">
+                  <Legend className='mb-1 flex justify-end'
+                    categories={genomeNames}
+                    colors={predefinedColors}
+                  />
+                  <Grid numItems={numItemsInGrid} className="gap-2">
                     {Object.entries(dataByPI).map(([pi, rows]) => {
                       const chartData = rows.reduce((result, row) => {
                         const projectIndex = result.findIndex(item => item.name === row.project);
@@ -154,32 +151,32 @@ const CustomBarChart3 = () => {
                       const colors = Object.values(genomeColors);
 
                       return (
-                        <Card key={pi}>
-                          <Title>{pi}</Title>
+                        <Card key={pi} className='pl-1'>
+
                           <BarChart
                             className={cardSize}
                             data={chartData}
                             index="name"
                             categories={Object.keys(genomeColors)}
                             colors={colors}
-                            yAxisWidth={48}
+                            yAxisWidth={44}
                             showLegend={false}
                             stack={true}
                             showXAxis={false}
                           />
+                          <Title className='flex justify-center'>{pi}</Title>
                         </Card>
                       );
                     })}
-
-
                   </Grid>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                  <Button onClick={zoomIn}>Zoom In</Button>
-                  <Button onClick={zoomOut}>Zoom Out</Button>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex space-x-4">
+
                   <Button icon={XIcon} className="bg-gray-700 text-white hover:bg-gray-500 border-none mx-auto" onClick={closeModal}>
                     Go back
                   </Button>
+                  <Button icon={MinusCircleIcon} className='bg-gray-700 text-white hover:bg-gray-500 border-none' onClick={minus}>Zoom Out</Button>
+                  <Button icon={PlusCircleIcon} className='bg-gray-700 text-white hover:bg-gray-500 border-none' onClick={plus}>Zoom In</Button>
                 </div>
               </div>
             </Transition.Child>
