fd8ac5d5e86211714403d10fd955e98ba1207270
Deep Chandra <deepc@andrew.cmu.edu>
2023-07-27 06:08:13 +0300

    all running smooth

--- front-end/src/pages/progress.jsx
+++ front-end/src/pages/progress.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Card, Table, TableRow, TableCell, TableHead, TableHeaderCell, TableBody, BadgeDelta, MultiSelect, MultiSelectItem }
+import { Card, Table, TableRow, TableCell, TableHead, TableHeaderCell, TableBody, Badge, MultiSelect, MultiSelectItem }
   from "@tremor/react";
-
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/solid";
 const data = require('../newdata/data0.json');
 
 const Progress = (props) => {
@@ -28,25 +28,34 @@ const Progress = (props) => {
   // }, [page]);
 
   const [selectedSamples, setSelectedSamples] = useState([]);
-  // const [selectedFlowcells, setSelectedFlowcells] = useState([]);
-  // const [selectedSubmissions, setSelectedSubmissions] = useState([]);
+  const [selectedFlowcells, setSelectedFlowcells] = useState([]);
+  const [selectedSubmissions, setSelectedSubmissions] = useState([]);
+  const [selectedDdates, setSelectedDdates] = useState([]);
+  const [selectedSdates, setSelectedSdates] = useState([]);
 
   const isSampleSelected = (item) =>
     selectedSamples.includes(item.sample_id) || selectedSamples.length === 0;
 
-  // const isFlowcellSelected = (item) =>
-  //   selectedFlowcells.includes(item.fc_id) || selectedFlowcells.length === 0;
+  const isFlowcellSelected = (item) =>
+    selectedFlowcells.includes(item.fc_id) || selectedFlowcells.length === 0;
 
-  // const isSubmissionSelected = (item) =>
-  //   selectedSubmissions.includes(item.submission_id) || selectedSubmissions.length === 0;
+  const isSubmissionSelected = (item) =>
+    selectedSubmissions.includes(item.submission_id) || selectedSubmissions.length === 0;
+
+  const isDdateSelected = (item) =>
+    selectedDdates.includes(item.loading_date) || selectedDdates.length === 0;
+
+  const isSdateSelected = (item) =>
+    selectedSdates.includes(item.staging_date) || selectedSdates.length === 0;
 
   return (
-    <Card>
+    <div>
 
-      {data && (<Table className="mt-6">
+      {data && (<Table className="pt-6 min-h-screen">
         <TableHead>
           <TableRow>
-            <TableHeaderCell>
+            <TableHeaderCell />
+            <TableHeaderCell className="z-10">
               <MultiSelect
                 onValueChange={setSelectedSamples}
                 placeholder="Select Samples..."
@@ -60,70 +69,97 @@ const Progress = (props) => {
               </MultiSelect>
             </TableHeaderCell>
 
-            {/* <TableHeaderCell>
+            <TableHeaderCell className="z-10">
               <MultiSelect
                 onValueChange={setSelectedFlowcells}
-                placeholder="Select Regions..."
+                placeholder="Select Flowcells..."
                 className="max-w-xs"
               >
-                {Array.from(new Set(data.map((item) => item.region))).map((region) => (
-                  <MultiSelectItem key={region} value={region}>
-                    {region}
+                {Array.from(new Set(data.map((item) => item.fc_id))).map((fc_id) => (
+                  <MultiSelectItem key={fc_id} value={fc_id}>
+                    {fc_id}
                   </MultiSelectItem>
                 ))}
               </MultiSelect>
             </TableHeaderCell>
 
-            <TableHeaderCell>
+            <TableHeaderCell className="z-10">
               <MultiSelect
                 onValueChange={setSelectedSubmissions}
-                placeholder="Select Status..."
+                placeholder="Select Submissions..."
                 className="max-w-xs"
               >
-                {Array.from(new Set(data.map((item) => item.status))).map((status) => (
-                  <MultiSelectItem key={status} value={status}>
-                    {status}
+                {Array.from(new Set(data.map((item) => item.submission_id))).map((submission_id) => (
+                  <MultiSelectItem key={submission_id} value={submission_id}>
+                    {submission_id}
                   </MultiSelectItem>
                 ))}
               </MultiSelect>
-            </TableHeaderCell> */}
+            </TableHeaderCell>
+            <TableHeaderCell className="z-10">
+              <MultiSelect
+                onValueChange={setSelectedDdates}
+                placeholder="Select Date..."
+                className="max-w-xs"
+              >
+                {Array.from(new Set(data.map((item) => item.loading_date))).map((loading_date) => (
+                  <MultiSelectItem key={loading_date} value={loading_date}>
+                    {loading_date}
+                  </MultiSelectItem>
+                ))}
+              </MultiSelect>
+            </TableHeaderCell>
+            <TableHeaderCell className="z-10">
+              <MultiSelect
+                onValueChange={setSelectedSdates}
+                placeholder="Select Date..."
+                className="max-w-xs"
+              >
+                {Array.from(new Set(data.map((item) => item.staging_date))).map((staging_date) => (
+                  <MultiSelectItem key={staging_date} value={staging_date}>
+                    {staging_date}
+                  </MultiSelectItem>
+                ))}
+              </MultiSelect>
+            </TableHeaderCell>
           </TableRow>
           <TableRow>
+            <TableHeaderCell>S.No.</TableHeaderCell>
             <TableHeaderCell>Sample ID</TableHeaderCell>
-            <TableHeaderCell className="text-right">Flowcell ID</TableHeaderCell>
-            <TableHeaderCell className="text-right">Submission ID ($)</TableHeaderCell>
-            <TableHeaderCell className="text-right">Demultiplexed</TableHeaderCell>
-            <TableHeaderCell className="text-right">Staged</TableHeaderCell>
-            <TableHeaderCell className="text-right">Lane fastq</TableHeaderCell>
-            <TableHeaderCell className="text-right">Merged fastq</TableHeaderCell>
+            <TableHeaderCell className="">Flowcell ID</TableHeaderCell>
+            <TableHeaderCell className="">Submission ID ($)</TableHeaderCell>
+            <TableHeaderCell className="">Demultiplexed</TableHeaderCell>
+            <TableHeaderCell className="">Staged</TableHeaderCell>
+            <TableHeaderCell className="">Lane fastq</TableHeaderCell>
+            <TableHeaderCell className="">Merged fastq</TableHeaderCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
           {data
-            .filter((item) => isSampleSelected(item))
-            .map((item) => (
+            .filter((item) => (isSampleSelected(item) && isFlowcellSelected(item) && isSubmissionSelected(item) && isDdateSelected(item) && isSdateSelected(item)))
+            .map((item, index) => (
               <TableRow key={item.sample_id}>
+                <TableCell>{index + 1}</TableCell>
                 <TableCell>{item.sample_id}</TableCell>
-                <TableCell className="text-right">{item.fc_id}</TableCell>
-                <TableCell className="text-right">{item.submission_id}</TableCell>
-                <TableCell className="text-right">{item.loading_date}</TableCell>
-                <TableCell className="text-right">{item.loading_date}</TableCell>
-                <TableCell className="text-right">
-                  <BadgeDelta deltaType="moderateIncrease" size="xs">
-                    {item.loading_date}
-                  </BadgeDelta>
+                <TableCell className="">{item.fc_id}</TableCell>
+                <TableCell className="">{item.submission_id}</TableCell>
+                <TableCell className="">{item.loading_date ?
+                  (<Badge icon={CheckCircleIcon} color="emerald" size="xs">{item.loading_date}</Badge>)
+                  : (<Badge icon={XCircleIcon} color="red" size="xs">Not demultiplexed </Badge>)}</TableCell>
+                <TableCell className="">{item.staging_date ?
+                  (<Badge icon={CheckCircleIcon} color="emerald" size="xs">{item.staging_date}</Badge>)
+                  : (<Badge icon={XCircleIcon} color="red" size="xs">Not staged </Badge>)}
                 </TableCell>
-                <TableCell className="text-right">
-                  <BadgeDelta deltaType="unchanged" size="xs">
-                    {item.loading_date}
-                  </BadgeDelta>
+                <TableCell className="">
+                </TableCell>
+                <TableCell className="">
                 </TableCell>
               </TableRow>
             ))}
         </TableBody>
       </Table>)}
-    </Card>
+    </div>
   );
 }
 
