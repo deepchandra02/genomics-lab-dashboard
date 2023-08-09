ba2c252e3902d6d4038a4925b850619dd28ce804
Deep Chandra <deepc@andrew.cmu.edu>
2023-08-09 08:05:22 +0300

    semi-working filter

--- front-end/src/components/shared/Sidebar.jsx
+++ front-end/src/components/shared/Sidebar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import classNames from 'classnames'
-import { Divider } from '@tremor/react'
+import { useFilters, Filters } from '../../pages/Progress'
+import { Accordion, AccordionList, AccordionHeader, Button, Divider, Icon } from '@tremor/react'
 import { Link, useLocation } from 'react-router-dom'
-import { TrendingUpIcon, ChartBarIcon, CogIcon, LogoutIcon, MenuIcon, XIcon } from '@heroicons/react/solid'
+import { TrendingUpIcon, ChartBarIcon, ChevronRightIcon, CogIcon, LogoutIcon, MenuIcon, XIcon, ChevronLeftIcon } from '@heroicons/react/solid'
+const data = require('../../data/data0_updated.json');
 
 const DASHBOARD_SIDEBAR_LINKS = [
   {
@@ -28,23 +30,59 @@ const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
   }
 ]
 
-const linkClass = 'flex items-center w-52 gap-2 font-semibold text-base py-3 pl-4 transition-colors duration-200 ease-in-out hover:bg-neutral-500'
+const linkClass = 'flex justify-between items-center w-72 gap-2 font-semibold font-cabin py-3 pl-4 hover:bg-gray-300'
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
+  const [filter, setFilter] = useState(false);
+  function SidebarLink({ link }) {
+    const { pathname } = useLocation()
 
+    return (
+      <Link
+        to={link.path}
+        className={classNames(pathname === link.path ? 'bg-gray-100 text-teal-700' : 'text-teal-600', linkClass)}
+      >
+        <div className="flex">
+          {link.icon}
+          <span className="ml-2">{link.label}</span>
+        </div>
+        {link.path === '/progress' && pathname === link.path && (<Button
+          className={"mr-4 p-2 h-6 bg-slate-700 hover:bg-slate-500 border-none"}
+          variant="primary"
+          onClick={() => setFilter(true)}
+        >
+          <Icon
+            className="text-white"
+            variant="simple"
+            icon={ChevronRightIcon}
+          />
+        </Button>)}
+      </Link>
+    )
+  }
   return (
     <>
-      <div className={classNames("bg-white font-cabin text-slate-800 w-52 h-screen py-6 flex flex-col ", { 'hidden': !isOpen })}>
-        <div className="flex justify-between items-center px-4 mb-8">
-          <h1 className="text-3xl font-bold">Sidra</h1>
-          <XIcon className="h-6 w-6 cursor-pointer" onClick={() => setIsOpen(false)} />
-        </div>
-        <div className="flex-1 space-y-4">
-          {DASHBOARD_SIDEBAR_LINKS.map((link) => (
-            <SidebarLink key={link.key} link={link} />
-          ))}
-        </div>
+      <div className={classNames("bg-white font-cabin text-slate-800 w-72 h-screen py-6 flex flex-col justify-between", { 'hidden': !isOpen })}>
+        {!filter && (
+          <div className="flex flex-col">
+            <div className="flex justify-between items-center px-4 mb-8">
+              <h1 className="text-3xl font-bold">Sidra</h1>
+              <XIcon className="h-6 w-6 cursor-pointer" onClick={() => setIsOpen(false)} />
+            </div>
+            <div className="flex-1 space-y-4">
+              {DASHBOARD_SIDEBAR_LINKS.map((link) => (
+                <SidebarLink key={link.key} link={link} />
+              ))}
+            </div>
+          </div>
+        )}
+        {filter && (
+          <Filters
+            setFilter={setFilter}
+            setIsOpen={setIsOpen}
+          />
+        )}
         <div className="mt-6">
           <div className="px-4">
             <Divider />
@@ -53,26 +91,16 @@ export default function Sidebar() {
             <SidebarLink key={link.key} link={link} />
           ))}
           <div className={classNames(linkClass, 'cursor-pointer text-red-500 hover:text-red-400')}>
-            <LogoutIcon className="h-6 w-6" />
-            <span className="ml-2">Logout</span>
+            <div className="flex">
+              <LogoutIcon className="h-6 w-6" />
+              <span className="ml-2">Logout</span>
+            </div>
           </div>
         </div>
       </div>
-      {!isOpen && (<MenuIcon className="h-6 w-6 cursor-pointer fixed top-4 left-4 z-50" onClick={() => setIsOpen(true)} />)}
+      {!isOpen && (<MenuIcon className="h-6 w-6 cursor-pointer fixed top-4 left-2 z-50" onClick={() => setIsOpen(true)} />)}
     </>
   )
 }
 
-function SidebarLink({ link }) {
-  const { pathname } = useLocation()
 
-  return (
-    <Link
-      to={link.path}
-      className={classNames(pathname === link.path ? 'bg-gray-100 text-teal-700' : 'text-teal-600', linkClass)}
-    >
-      {link.icon}
-      <span className="ml-2">{link.label}</span>
-    </Link>
-  )
-}
